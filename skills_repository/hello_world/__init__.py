a99dd0b590fa001c43ac09f8bb9380f710cd43cf
aamott <adam.amott@gmail.com>
2022-02-03 20:19:22 -0700

    Added intent to hello_world skill

--- skills_repository/hello_world/__init__.py
+++ skills_repository/hello_world/__init__.py
@@ -3,17 +3,17 @@ from skills_repository.alfred_skill import AlfredSkill
 class HelloWorld(AlfredSkill):
     name="Hello World"
     
-    def __init__(self, alfred_core):
+    def __init__(self, alfred_utils):
         """Initialize the Hello World skill class.
             All skills should have an init method which receives
-            an instance of AlfredCore. To find out more about 
-            AlfredCore, read its class file.  
+            an instance of AlfredUtils. To find out more about 
+            AlfredUtils, read its class file.  
 
         Args:
-            alfred_core (class): Class holding essential tools, including say(phrase) and listen()
+            alfred_utils (class): Class holding essential tools, including say(phrase) and listen()
         """
         AlfredSkill.__init__(self)
-        self._alfred_core = alfred_core
+        self._alfred_utils = alfred_utils
 
 
     def initialize_intents(self, register_intent):
@@ -22,6 +22,7 @@ class HelloWorld(AlfredSkill):
         Receives: a reference to AlfredProtocol's intent register function
         """
         register_intent(self.handle_say_hello, ["hello", "hello world"], self)
+        register_intent(self.handle_repeat_after_me, ["repeat", "repeat after me"], self)
 
     
     def handle_say_hello(self, phrase):
@@ -30,18 +31,22 @@ class HelloWorld(AlfredSkill):
         Args:
             phrase (string): The phrase said that resulted in this function being called
         """
-        self._alfred_core.say("hello world!")
+        self._alfred_utils.say("hello world!")
+
+    def handle_repeat_after_me(self, phrase):
+        new_phrase = self._alfred_utils.listen()
+        self._alfred_utils.say(new_phrase)
 
 
 
-def create_skill(alfred_core):
+def create_skill(alfred_utils):
     """ Skill creator function. Creates an instance of your skill to return. 
 
     Args:
-        alfred_core (AlfredCore): class instance with essential Alfred tools
+        alfred_utils (AlfredUtils): class instance with essential Alfred tools
 
     Returns:
         HelloWorld: An instance of the HelloWorld skill
     """
-    skill_instance =  HelloWorld(alfred_core)
+    skill_instance =  HelloWorld(alfred_utils)
     return skill_instance
