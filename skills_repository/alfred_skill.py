eed20524164675bf9d4dd1a3223ac8d5754ddbe7
aamott <adam.amott@gmail.com>
2022-02-14 09:52:10 -0700

    Minor reformatting

--- skills_repository/alfred_skill.py
+++ skills_repository/alfred_skill.py
@@ -3,8 +3,7 @@ from abc import ABC, abstractmethod, abstractproperty
 class AlfredSkill(ABC):
     @abstractmethod 
     def initialize_intents(self, register_intent):
-        """
-        registers intents to AlfredProtocol
+        """ registers intents to AlfredProtocol
 
         Receives: a reference to AlfredProtocol's intent register function
         """
